ea87e44fdbd68d144b23d91fc859e7a5a613328e
Deonn Almirol <deonn.almirol@gmail.com>
2023-06-16 13:42:29 -0700

    bulk of program developed

--- main.c
+++ main.c
@@ -2,24 +2,159 @@
 #include<string.h>
 #include<time.h>
 
+// initial values
 double walletBalance = 10.00;
-double coffeePrice = 0.50;
 
-void wallet() {
-	printf("Your current wallet balance is $%f\n", walletBalance);
+struct shopItem {
+	char name[15];
+	double price;
+};
+
+struct shopItem items[7];
+
+char itemNames[7][10] = {"Coffee", "Croissant", "Tea", "Muffin", "Donut", "Pastry", "Water"};
+double itemPrices[7] = {0.50, 2.00, 1.00, 1.50, 1.25, 2.50, 0.00};
+
+void initItems();
+void wallet();
+void purchaseItem(int itemNum);
+void showPrices();
+
+int main() {
+	
+	initItems();
+
+	for (int i = 0; i<=6; i++) {
+		printf("%d %s: $%.2f\n", i, items[i].name, items[i].price);
+	}
+
+
+	char option;
+	int desiredItem;
+
+	printf("What would you like to do? Available options are: p for purchase, w for wallet, q for quit, s to show prices... ");
+
+	while(1) {		
+		scanf(" %c", &option);
+
+		if (option == 'p') {
+
+			printf("What would you like to purchase? enter the number corresponding to the item. for example, if you would like to buy coffee, press 0 and press enter...\n");
+			scanf(" %d", &desiredItem);
+			if (desiredItem >= 0 && desiredItem <= 6) {
+				purchaseItem(desiredItem);
+			} else {
+				printf("that item does not exist!\n");
+			}
+
+		} else if(option == 'w') {
+
+			wallet();
+
+		} else if(option == 'q') {
+
+			break;
+
+		} else if(option == 's') {
+
+			showPrices();
+
+		} else {
+			printf("invalid argument! if you would like to quit, please type q\n");
+		}
+	}
+
+	return 0;
 }
 
-void purchaseItem() {
+void initItems() {
+	for (int i = 0; i<=6; i++) {
+		strcpy(items[i].name, itemNames[i]);
+		items[i].price = itemPrices[i];
+	}
+}
+
+void wallet() {
+	printf("Your current wallet balance is $%.2f\n", walletBalance);
+}
+
+void purchaseItem(int itemNum) {
 	clock_t start, end;
 	start = clock();
 
-	printf("purchasing coffee for $0.50...\n");
-	printf("withdrawing $0.50 from wallet with balance of $%f\n", walletBalance);
-	if (walletBalance >= coffeePrice) {
-		walletBalance -= coffeePrice;
-		printf("Success! Your new balance is %f\n", walletBalance);
-	} else {
-		printf("you're too broke to purchase this!\n");
+	switch(itemNum) {
+		case 0:
+			printf("purchasing %s for $%.2f...\n", items[0].name, items[0].price);
+			printf("withdrawing $%.2f from wallet with a balance of $%.2f\n", items[0].price, walletBalance);
+			if(walletBalance >= items[0].price) {
+				walletBalance -= items[0].price;
+				printf("success! your new balance is $%.2f\n", walletBalance);
+			} else {
+				printf("you are too broke for this!\n");
+			}
+			break;
+		case 1:
+			printf("purchasing %s for $%.2f...\n", items[1].name, items[1].price);
+			printf("withdrawing $%.2f from wallet with a balance of $%.2f\n", items[1].price, walletBalance);
+			if(walletBalance >= items[1].price) {
+				walletBalance -= items[1].price;
+				printf("success! your new balance is $%.2f\n", walletBalance);
+			} else {
+				printf("you are too broke for this!\n");
+			}
+			break;
+		case 2:
+			printf("purchasing %s for $%.2f...\n", items[2].name, items[2].price);
+			printf("withdrawing $%.2f from wallet with a balance of $%.2f\n", items[2].price, walletBalance);
+			if(walletBalance >= items[2].price) {
+				walletBalance -= items[2].price;
+				printf("success! your new balance is $%.2f\n", walletBalance);
+			} else {
+				printf("you are too broke for this!\n");
+			}
+			break;
+		case 3:
+			printf("purchasing %s for $%.2f...\n", items[3].name, items[3].price);
+			printf("withdrawing $%.2f from wallet with a balance of %.2f\n", items[3].price, walletBalance);
+			if(walletBalance >= items[3].price) {
+				walletBalance -= items[3].price;
+				printf("success! your new balance is %.2f\n", walletBalance);
+			} else {
+				printf("you are too broke for this!\n");
+			}
+			break;
+		case 4:
+			printf("purchasing %s for $%.2f...\n", items[4].name, items[4].price);
+			printf("withdrawing $%.2f from wallet with a balance of %.2f\n", items[4].price, walletBalance);
+			if(walletBalance >= items[4].price) {
+				walletBalance -= items[4].price;
+				printf("success! your new balance is %.2f\n", walletBalance);
+			} else {
+				printf("you are too broke for this!\n");
+			}
+			break;
+		case 5:
+			printf("purchasing %s for $%.2f...\n", items[5].name, items[5].price);
+			printf("withdrawing $%.2f from wallet with a balance of %.2f\n", items[5].price, walletBalance);
+			if(walletBalance >= items[5].price) {
+				walletBalance -= items[5].price;
+				printf("success! your new balance is %.2f\n", walletBalance);
+			} else {
+				printf("you are too broke for this!\n");
+			}
+			break;
+		case 6:
+			printf("purchasing %s for $%.2f...\n", items[6].name, items[6].price);
+			printf("withdrawing $%.2f from wallet with a balance of %.2f\n", items[6].price, walletBalance);
+			if(walletBalance >= items[6].price) {
+				walletBalance -= items[6].price;
+				printf("success! your new balance is %.2f\n", walletBalance);
+			} else {
+				printf("you are too broke for this!\n");
+			}
+			break;
+		default:
+			printf("that item does not exist!");
 	}
 
 	end = clock();
@@ -28,24 +163,9 @@ void purchaseItem() {
 }
 
 void showPrices() {
-	printf("coffee: $%f\n", coffeePrice);
-}
 
-int main() {
-	char option;
-
-	printf("coffee: $0.50\nWhat would you like to do? Available options are: p for purchase, w for wallet, q for quit, s to show prices... ");
-
-	while(1) {		
-		scanf("%c", &option);
-		if (option == 'p') {
-			purchaseItem();
-		} else if(option == 'w') {
-			wallet();
-		} else if(option == 'q') {
-			break;
-		}
+	for (int i = 0; i<=6; i++) {
+		printf("%d %s: $%.2f\n", i, items[i].name, items[i].price);
 	}
 
-	return 0;
 }
